ddb081c08c5c2e564b5955519566f0ba768820c4
Nacho Ledesma <nacho.ledesma@example.com>
2024-08-25 19:47:39 -0300

    fix file brevo.ts and add changes in actions.tsx

--- src/app/action.ts
+++ src/app/action.ts
@@ -3,23 +3,22 @@ import { sendSmtpEmail } from "./lib/brevo";
 export async function handleForm(formData: FormData) {
   "use server";
   const title = formData.get("title");
-  console.log({ title });
   const to_name = formData.get("to_name");
-  const to_email = formdata.get("to_email");
+  const to_email = formData.get("to_email");
   const content = formData.get("content");
 
   if (!title || !to_name || !to_email || !content) {
     return console.log("Please fill all fields");
   }
 
-  console.log({ title, to, content });
+  console.log({ title, to_name, to_email, content });
 
   await sendSmtpEmail({
     subject: title as string,
     to: [
       {
-        email: to as string,
-        name: "Nacho Ledesma",
+        email: to_name as string,
+        name: to_email as string,
       },
     ],
 
--- src/app/lib/brevo.ts
+++ src/app/lib/brevo.ts
@@ -1,27 +1,45 @@
 import * as brevo from "@getbrevo/brevo";
 
 const instanseApi = new brevo.TransactionalEmailsApi();
+const api_key ="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"
 
 instanseApi.setApiKey(
   brevo.TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY as string
+  api_key as string
 );
-
-const smtpEmail = new brevo.SendSmtpEmail();
 
 interface Params {
   subject: string;
   to: { email: string; name: string }[];
   htmlContent: string;
 }
-export async function sendSmtpEmail({ subject, to, htmlContent }) {
+
+export async function sendSmtpEmail({ subject, to, htmlContent }: Params) {
+  const smtpEmail = new brevo.SendSmtpEmail();
+
   smtpEmail.subject = subject;
   smtpEmail.to = to;
-  smtpEmail.htmlContent = htmlContent;
+  smtpEmail.htmlContent = `<html><body>${htmlContent}</body></html>`;
   smtpEmail.sender = {
     name: "Nacho Ledesma",
     email: "nacholedesma33@gmail.com",
   };
 
-  await instanseApi.sendTransacEmail(smtpEmail);
+  try {
+    const response = await instanseApi.sendTransacEmail(smtpEmail);
+    console.log("API response:", response);
+  } catch (e) {
+    console.error("Error during email sending process:");
+
+    if (e instanceof Error) {
+      console.error("Error message:", e.message);
+    }
+
+    if (typeof e === "object" && e !== null && "response" in e) {
+      const response = (e as any).response;
+      console.error("Response status:", response?.status);
+      console.error("Response data:", response?.data);
+    }
+  }
 }
+
